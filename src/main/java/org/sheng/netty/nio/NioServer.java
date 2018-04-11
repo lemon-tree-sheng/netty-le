b760b5877a04dde997e0c56412e7fd78777fd68d
shengxingyue <shengxingyue@souche.com>
2018-04-11 17:01:02 +0800

    feat:add nioServer

--- src/main/java/org/sheng/netty/nio/NioServer.java
+++ src/main/java/org/sheng/netty/nio/NioServer.java
@@ -1,7 +1,5 @@
 package org.sheng.netty.nio;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -11,113 +9,123 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
-/**
- * @author shengxingyue, created on 2018/1/18
- */
-@Slf4j
 public class NioServer {
-
-    /**
-     * 通道监控器
-     */
+    // 通道管理器
     private Selector selector;
 
-    NioServer() throws IOException {
-        initServer(8000);
-    }
-
+    /**
+     * 获得一个ServerSocket通道，并对该通道做一些初始化的工作
+     *
+     * @param port 绑定的端口号
+     * @throws IOException
+     */
     public void initServer(int port) throws IOException {
-        // 初始化服务端 channel
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.configureBlocking(false);
-        serverSocketChannel.socket().bind(new InetSocketAddress(port));
-
-        // 监控器绑定服务端 channel
-        selector = Selector.open();
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-        log.info("初始化服务端成功");
+        // 获得一个ServerSocket通道
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        // 设置通道为非阻塞
+        serverChannel.configureBlocking(false);
+        // 将该通道对应的ServerSocket绑定到port端口
+        serverChannel.socket().bind(new InetSocketAddress(port));
+        // 获得一个通道管理器
+        this.selector = Selector.open();
+        // 将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_ACCEPT事件,注册该事件后，
+        // 当该事件到达时，selector.select()会返回，如果该事件没到达selector.select()会一直阻塞。
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
     }
 
     /**
-     * 服务端监听
+     * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，则进行处理
      *
      * @throws IOException
      */
     public void listen() throws IOException {
+        System.out.println("服务端启动成功！");
+        // 轮询访问selector
         while (true) {
-            log.info("服务端监听中");
-            // 阻塞监听注册事件
+            // 当注册的事件到达时，方法返回；否则,该方法会一直阻塞
             selector.select();
+            // 获得selector中选中的项的迭代器，选中的项为注册的事件
+            Iterator<?> ite = this.selector.selectedKeys().iterator();
+            while (ite.hasNext()) {
+                SelectionKey key = (SelectionKey) ite.next();
+                // 删除已选的key,以防重复处理
+                ite.remove();
 
-            // 监听到注册事件
-            Iterator iterator = selector.selectedKeys().iterator();
-            while (iterator.hasNext()) {
-                SelectionKey selectionKey = (SelectionKey) iterator.next();
-                // 移除监听到的事件
-                iterator.remove();
-                // 处理监听到的事件
-                handleSelectionKey(selectionKey);
+                handler(key);
             }
         }
     }
 
     /**
-     * 处理监听到的事件
+     * 处理请求
      *
-     * @param selectionKey
+     * @param key
      * @throws IOException
      */
-    private void handleSelectionKey(SelectionKey selectionKey) throws IOException {
-        if (selectionKey.isAcceptable()) {
-            // 处理读事件
-            handleAccept(selectionKey);
-        } else if (selectionKey.isReadable()) {
-            // 处理客户端连接事件
-            handleRead(selectionKey);
+    public void handler(SelectionKey key) throws IOException {
+        // 客户端请求连接事件
+        if (key.isAcceptable()) {
+            handlerAccept(key);
+            // 获得了可读的事件
+        } else if (key.isReadable()) {
+            handelerRead(key);
         }
     }
 
     /**
-     * 处理读事件
-     * 事件处理完之后不再阻塞读
+     * 处理连接请求
      *
-     * @param selectionKey
+     * @param key
      * @throws IOException
      */
-    private void handleRead(SelectionKey selectionKey) throws IOException {
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        // 读是读的客户端 channel, 所以强转成客户端 channel
-        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-        int read = socketChannel.read(byteBuffer);
+    public void handlerAccept(SelectionKey key) throws IOException {
+        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+        // 获得和客户端连接的通道
+        SocketChannel channel = server.accept();
+        // 设置成非阻塞
+        channel.configureBlocking(false);
+
+        // 在这里可以给客户端发送信息哦
+        System.out.println("新的客户端连接");
+        // 在和客户端连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限。
+        channel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+    /**
+     * 处理读的事件
+     *
+     * @param key
+     * @throws IOException
+     */
+    public void handelerRead(SelectionKey key) throws IOException {
+        // 服务器可读取消息:得到事件发生的Socket通道
+        SocketChannel channel = (SocketChannel) key.channel();
+        // 创建读取的缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int read = channel.read(buffer);
         if (read > 0) {
-            byte[] bytes = byteBuffer.array();
-            log.info(new String(bytes).trim());
+            byte[] data = buffer.array();
+            String msg = new String(data).trim();
+            System.out.println("服务端收到信息：" + msg);
 
-            // 会写到客户端
-            socketChannel.write(ByteBuffer.wrap("服务端已经接收到消息".getBytes()));
+            //回写数据
+            ByteBuffer outBuffer = ByteBuffer.wrap("好的".getBytes());
+            channel.write(outBuffer);// 将消息回送给客户端
         } else {
-            socketChannel.close();
+            System.out.println("客户端关闭");
+            key.cancel();
         }
     }
 
     /**
-     * 处理客户端连接事件
+     * 启动服务端测试
      *
-     * @param selectionKey
      * @throws IOException
      */
-    private void handleAccept(SelectionKey selectionKey) throws IOException {
-        log.info("客户端连接成功");
-        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
-        SocketChannel socketChannel = serverSocketChannel.accept();
-
-        // 客户端 channel 上注册读事件
-        socketChannel.configureBlocking(false);
-        socketChannel.register(selector, SelectionKey.OP_READ);
-    }
-
     public static void main(String[] args) throws IOException {
-        NioServer nioServer = new NioServer();
-        nioServer.listen();
+        NioServer server = new NioServer();
+        server.initServer(8000);
+        server.listen();
     }
+
 }
